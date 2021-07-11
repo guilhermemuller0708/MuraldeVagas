93918faf6ce3164872f41e606a10da05ff2ebef4
guilherme070812 <54514054+Guilherme070812@users.noreply.github.com>
2021-07-10 22:19:55 -0300

    Added config redux vacancy

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { authSlice } from 'app/modules/Auth/redux/slice';
+import { vacancySlice } from 'app/modules/Vacancy/redux/vacancy/slice';
 
 export const reducers = combineReducers({
-  auth: authSlice.reducer
+  auth: authSlice.reducer,
+  vacancy: vacancySlice.reducer
 });
